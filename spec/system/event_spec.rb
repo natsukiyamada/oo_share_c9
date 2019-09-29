232b4e8646e6d28ddc586caf58ed0146405b6f0a
natsuki.yamada <ec2-user@ip-172-31-29-121.ec2.internal>
2019-09-29 07:59:49 +0000

    Rspecでログイン機能確認＆成功

--- spec/system/event_spec.rb
+++ spec/system/event_spec.rb
@@ -1,23 +1,28 @@
 require 'rails_helper'
 
-describe 'イベント管理機能' type: :system do
+describe 'イベント管理機能', type: :system do
     describe 'イベント一覧画面を表示する機能' do
         before do
           #test_user_a を作成しておく
           test_user_a = FactoryBot.create(:user, name: 'test_user_a', mail: 'a@example.com')
           #作成者がtest_user_aであるイベントを作成しておく
-          FactoryBot.create(:task, name: '最初のイベント', user: test_user_a)
+          FactoryBot.create(:event, name: '最初のイベント', user: test_user_a)
         end
         
-        contect 'test_user_aがログインしている時' do 
+        context 'test_user_aがログインしている時' do 
           before do 
               #test_user_aでログインする
               visit root_path
-              
+              find(class: 'log_in').click
+              sleep 0.5
+              fill_in 'login_mail_field', with: 'a@example.com'
+              fill_in 'login_password_field', with: 'password'
+              click_button 'login_btn'
           end
           
           it 'test_user_aが作成したイベントが表示される' do 
               #作成済みのタスクの名称が画面上に表示されていることを確認
+              expect(page).to have_content '最初のイベント'
           end
         end
     end
