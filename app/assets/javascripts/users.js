d48cb456601e0802886d225cf4a0ea5c3b89e3f6
natsuki.yamada <ec2-user@ip-172-31-29-121.ec2.internal>
2019-10-13 15:29:30 +0000

    views/users/show jqueryのアニメーション改善

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,7 +2,7 @@
 $(document).on('turbolinks:load', function() { 
   $(function(){
     $('.user_edit').hover(function(){
-        $( ".dropdown_menu_user_edit").slideDown('fast');
+        $(".dropdown_menu_user_edit:not(:animated)", this).slideDown('fast');
     }, function(){
         $(".dropdown_menu_user_edit").slideUp('fast');
     });
